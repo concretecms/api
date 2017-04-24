b5c5eb023b15ef733b0d29a55ce9b4d8d0c0a3ad
Michael Krasnow <mnkras@gmail.com>
2017-04-23 22:18:26 -0400

    Initial Commit

---
+++ sami_config.php
@@ -0,0 +1,35 @@
+<?php
+
+use Sami\Sami;
+use Sami\RemoteRepository\GitHubRemoteRepository;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+use Sami\Parser\Filter\TrueFilter;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('vendor')
+    ->in($dir = 'concrete5/concrete')
+;
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('8.*')
+    ->add('develop', 'develop branch')
+;
+
+$sami = new Sami($iterator, array(
+    // 'theme'                => 'symfony',
+    'versions'             => $versions,
+    'title'                => 'concrete5 API',
+    'build_dir'            => __DIR__.'/build/%version%',
+    'cache_dir'            => __DIR__.'/cache/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('concrete5/concrete5', dirname($dir)),
+    'default_opened_level' => 2,
+));
+
+$sami['filter'] = function () {
+    return new TrueFilter();
+};
+
+return $sami;
