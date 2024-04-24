581734831fe16c018cfb754aca926f7a048366b3
myq <myq@portlandlabs.com>
2024-04-24 12:21:05 -0700

    limit dev branches

--- config.php
+++ config.php
@@ -27,8 +27,6 @@ $versions = \Concrete5\Api\Version\PackagistVersonCollection::create('concrete5/
     // Only track 5.7.5.*, 8.*
     ->addFromSemver(['>=5.7.5 <8 || >8.2 || >9'])
     // Add the development version too
-    ->add('10.0.x-dev')
-    ->add('9.3.x-dev')
     ->add('9.2.x-dev');
 
 
